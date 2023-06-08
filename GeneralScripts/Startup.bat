03bc8c9cc4c3fb8e8b557b67219f9a338b459c64
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2023-06-07 23:41:08 -0500

    Wrote a batch file script that opens job websites that I need for my job search!

---
+++ GeneralScripts/Startup.bat
@@ -0,0 +1,6 @@
+set linkedin="https://www.linkedin.com/jobs/"
+set docs="https://docs.google.com/document/u/0/"
+set mail="https://mail.google.com/mail/u/0/#inbox"
+start chrome %linkedin%
+start chrome %docs%
+start chrome %mail%
